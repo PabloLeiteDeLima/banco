340fa0606c2f7f88c4570bacdce0296567a24ecf
Pablo Leite <pabloleitelima7@gmail.com>
2026-08-05 17:30:46 -0300

    Atualizando saldo e saque

--- banco.php
+++ banco.php
@@ -4,9 +4,47 @@
 echo "Digite o titular da conta: ";
 $nome = fgets(STDIN);
 echo "Saldo inicial: ";
-$saldo = fgets(STDIN);
+$saldo = (float) fgets(STDIN);
 
 //  -> Mostrar ao usuário conta aberta (titular e saldo inicial);
-echo "\n*************************";
-echo "\n=> Conta Aberta com sucesso!\n-> Titular: " . $nome . "-> Saldo inicial R$ " . $saldo . " reais";
-echo "*************************\n\n";
+echo "\n*********************************";
+echo "\n*** Conta Aberta com sucesso! ***\n";
+echo "-> Titular: " . $nome;
+echo "-> Saldo inicial R$ $saldo\n";
+echo "*********************************\n\n";
+
+//  -> Mostrar um menu:
+//      * 1. Verificar saldo;
+//      * 2. Saque (Tratar se conta tem saldo suficiente para o saque);
+//      * 3. Depositar;
+//      * 4. Sair;
+
+$opcao = 0;
+while($opcao != 4){
+    echo "1. Verificar saldo\n";
+    echo "2. Fazer saque\n";
+    echo "3. Fazer deposito\n";
+    echo "4. Sair do sistema\n";
+    echo "-> Escolha uma opção: ";
+    $opcao = fgets(STDIN);
+
+    // Tratar as opções escolhidas...
+    switch($opcao){
+        case 1:
+            echo "\n-> Saldo disponível R$ " . $saldo . "\n\n";
+            break;
+        case 2:
+            echo "\n-> Qual valor do saque: ";
+            $saque = fgets(STDIN);
+            if($saque <= $saldo){
+                echo "\nSeu saque foi no valor de R$ $saque";
+                echo "->Saque efetuado com sucesso!\n";
+                $saldo -= $saque;
+            }else{
+                echo "\n-> Saldo indisponível.\n";
+            }
+            echo "\n\n";
+            break;
+    }
+
+}
